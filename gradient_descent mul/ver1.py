0915063cc90d788386e2299483294e0718d6f521
bk_hunter <hoanganhvt.dev@gmail.com>
2024-12-13 14:09:39 +0700

    gg

--- gradient_descent mul/ver1.py
+++ gradient_descent mul/ver1.py
@@ -14,18 +14,18 @@ def gradient_descent(w,b,x,y,L):
 	[n,w_len]=x.shape
 	derivative_w=np.zeros(w_len,)
 	derivative_b=0.0
-	for i in range(n):
-		err=np.dot(w,x[i])+b-y[i]
-		derivative_b+=err
-		for j in range(w_len):
-			derivative_w[j]=derivative_w[j]+err*x[i,j]
-	derivative_w=derivative_w/n 
+	for i in range(w_len):
+		for j in range(n):
+			derivative_w[i]=derivative_w[i]+(np.dot(w,x[j])+b-y[j])*x[j,i]
+	derivative_w=derivative_w/float(n)
 	w=w-L*derivative_w
-	b=b-L*derivative_b
+	for i in range(n):
+		derivative_b+=(np.dot(w,x[i])+b-y[i])
+	b=b-L*derivative_b/float(n)
 	return [w,b]
 
-b_init = 0
-w_init = np.array([ 0, 0, 0, 0])
+b_init = 785.1811367994083
+w_init = np.array([ 0.39133535, 18.75376741, -53.36032453, -26.42131618])
 x_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
 y_train = np.array([460, 232, 178])
 for i in range(1000):
--- gradient_descent mul/ver2.py
+++ gradient_descent mul/ver2.py
@@ -13,23 +13,22 @@ def loss_function(w,b,x,y):
 def gradient_descent(w,b,x,y,L):
 	[n,w_len]=x.shape
 	derivative_w=np.zeros(w_len,)
-	derivative_b=0
+	derivative_b=0.0
 	for i in range(n):
-		err=np.dot(x[i],w)+b-y[i]
+		err=np.dot(w,x[i])+b-y[i]
 		derivative_b+=err
 		for j in range(w_len):
-			err=err*x[i,j]
-			derivative_w[i]+=err 
+			derivative_w[j]=derivative_w[j]+err*x[i,j]
 	derivative_w=derivative_w/n 
-	derivative_b=derivative_b/n 
-	w=w-L*derivative_b
+	w=w-L*derivative_w
 	b=b-L*derivative_b
 	return [w,b]
 
 b_init = 0
-w_init = np.array([ 0,0,0,0])
+w_init = np.array([ 0, 0, 0, 0])
 x_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
 y_train = np.array([460, 232, 178])
 for i in range(1000):
-	[w_init,b_init]=gradient_descent(w_init,b_init,x_train,y_train,0.001)
+	[w,b]=gradient_descent(w_init,b_init,x_train,y_train,0.00001)
+print(w,' ',b)
 print(loss_function(w_init,b_init,x_train,y_train))
