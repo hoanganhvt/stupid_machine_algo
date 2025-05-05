5972f46307855e4bea6f2b678a44ccb972314f04
bk_hunter <hoanganhvt.dev@gmail.com>
2025-05-05 12:22:12 +0700

    gg

--- gradient_descent mul/ver2.py
+++ gradient_descent mul/ver2.py
@@ -1,34 +1,54 @@
 import pandas as pd
-import numpy as np
+import numpy as np 
+import matplotlib.pyplot as plt
 
-def loss_function(w,b,x,y):
-	n=x.shape[0]
-	result=0.0
+def loss_function(x,y,w,m,n):
+	res=0
+	for i in range(m):
+		tmp=0
+		for j in range(n):
+			tmp=tmp+(x[i][j]*w[j]-y[i])**2
+		res+=tmp 
+	return res/(2*m)
+
+def linear_regression_gradient_descent(X,y,theta,alpha) -> np.ndarray:    
+	m, n = X.shape
+	derivatives=np.zeros(n)
 	for i in range(n):
-		tmp=(np.dot(x[i],w)+b-y[i])**2
-		result=tmp
-	result=result/(2*n)
-	return tmp
+		derivative=0
+		for j in range(m):
+			derivative=derivative+(np.dot(theta,X[j])-y[j])*X[j][i]
+		derivative/=m
+		derivatives[i]=derivative
+	theta=theta-derivatives*alpha
+	return theta
+	
+data=pd.read_csv("test.csv").to_dict()
+number_of_test=len(data['x'])
 
-def gradient_descent(w,b,x,y,L):
-	[n,w_len]=x.shape
-	derivative_w=np.zeros(w_len,)
-	derivative_b=0.0
-	for i in range(n):
-		err=np.dot(w,x[i])+b-y[i]
-		derivative_b+=err
-		for j in range(w_len):
-			derivative_w[j]=derivative_w[j]+err*x[i,j]
-	derivative_w=derivative_w/n 
-	w=w-L*derivative_w
-	b=b-L*derivative_b
-	return [w,b]
-
-b_init = 0
-w_init = np.array([ 0, 0, 0, 0])
-x_train = np.array([[2104, 5, 1, 45], [1416, 3, 2, 40], [852, 2, 1, 35]])
-y_train = np.array([460, 232, 178])
-for i in range(1000):
-	[w,b]=gradient_descent(w_init,b_init,x_train,y_train,0.00001)
-print(w,' ',b)
-print(loss_function(w_init,b_init,x_train,y_train))
+x=[]
+y=[]
+for q in range(number_of_test):
+	x.append([])
+	for j in data:
+		if j != "y":
+			x[q].append(data[j][q])
+		else:
+			y.append(data[j][q])
+x=np.array(x)
+y=np.array(y)
+w=np.dot(
+	np.linalg.pinv(
+		np.dot(
+			np.transpose(x),
+			x
+		)
+	),
+	np.dot(
+		np.transpose(x),
+		y 
+	)
+)
+plt.scatter(x,y)
+plt.plot(x, w * x, color='red')  
+plt.show()
--- main.py
+++ main.py
@@ -11,18 +11,16 @@ def loss_function(x,y,w,m,n):
 		res+=tmp 
 	return res/(2*m)
 
-def linear_regression_gradient_descent(X: np.ndarray, y: np.ndarray, alpha: float, iterations: int) -> np.ndarray:    
+def linear_regression_gradient_descent(X,y,theta,alpha) -> np.ndarray:    
 	m, n = X.shape
-	theta = np.zeros((n))
 	derivatives=np.zeros(n)
-	for q in range(iterations):
-		for i in range(n):
-			derivative=0
-			for j in range(m):
-				derivative=derivative+(np.dot(theta,X[j])-y[j])*X[j][i]
-			derivative/=m
-			derivatives[i]=derivative
-		theta=theta-derivatives*alpha
+	for i in range(n):
+		derivative=0
+		for j in range(m):
+			derivative=derivative+(np.dot(theta,X[j])-y[j])*X[j][i]
+		derivative/=m
+		derivatives[i]=derivative
+	theta=theta-derivatives*alpha
 	return theta
 	
 data=pd.read_csv("test.csv").to_dict()
@@ -39,9 +37,18 @@ for q in range(number_of_test):
 			y.append(data[j][q])
 x=np.array(x)
 y=np.array(y)
-m,n=x.shape
-w=np.zeros((n))
-w=linear_regression_gradient_descent(x,y,0.00001,1000)
+w=np.dot(
+	np.linalg.pinv(
+		np.dot(
+			np.transpose(x),
+			x
+		)
+	),
+	np.dot(
+		np.transpose(x),
+		y 
+	)
+)
 plt.scatter(x,y)
 plt.plot(x, w * x, color='red')  
 plt.show()
